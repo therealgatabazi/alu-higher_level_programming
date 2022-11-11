3972ced126dfe5bcc2adbad3571fac0bc2ebe335
therealgatabazi <d.gatabazi@alustudent.com>
2022-11-11 10:56:49 -0800

    Added file 2 2nd correction

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -39,6 +39,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """Return the area of the reactangle
         """
