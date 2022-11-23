4afce0feed1284bca55f37d9eab5c3cda0380f94
therealgatabazi <d.gatabazi@alustudent.com>
2022-11-23 09:33:44 +0000

    Added file 1 final correction

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 '''a python script that fetches a url'''
-
-
-
 import urllib.request
-
-
-
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
