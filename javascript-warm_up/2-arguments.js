660f426fe02c472c7b4b147973defea099bf2870
therealgatabazi <d.gatabazi@alustudent.com>
2023-02-03 14:02:16 +0000

    Saved changes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const value = process.argv.slice(2);
 
-if (value.length == 0) {
+if (value.length === 0) {
   console.log('No argument');
-} else if (value.length == 1) {
+} else if (value.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
