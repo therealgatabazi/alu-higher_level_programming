aeda07836b9342e3780357529721a8614aff7161
therealgatabazi <d.gatabazi@alustudent.com>
2023-02-03 16:29:22 +0200

    hmm

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const [value] = process.argv.slice(2);
 if (!value) {
-    console.log('No argument');
-}
-else {
-    console.log(value);
+  console.log('No argument');
+} else {
+  console.log(value);
 }
