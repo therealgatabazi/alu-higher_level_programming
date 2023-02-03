e28c1939a55320240903f71a4418e1cab5d6595c
therealgatabazi <d.gatabazi@alustudent.com>
2023-02-03 15:45:26 +0200

    first arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const value = process.argv.slice(2);
+
+
+if (value.length == 0) {
+  console.log('No argument');
+}
+else if (value.length == 1) {
+  console.log('Argument found');
+}
+
+else {
+    console.log('Arguments found');
+}
+
