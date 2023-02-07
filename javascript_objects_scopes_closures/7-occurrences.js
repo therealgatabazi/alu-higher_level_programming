63d456fb43edd0c5b648747cc316123ac4c08b7f
therealgatabazi <d.gatabazi@alustudent.com>
2023-02-07 16:31:14 +0200

    file 7

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// function that returns the number of occurences in a list
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
